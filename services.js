cc880ae5b57236a309250587a68e2c116eabac25
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2022-05-18 21:56:01 +0100

    made changes

--- services.js
+++ services.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
 export const originUrl =
   process.env.NODE_ENV === "production"
-    ? "https://tmpcouplesnight.web.app/"
+    ? "https://tmpcouplesnight.web.app"
     : "http://localhost:3000";
